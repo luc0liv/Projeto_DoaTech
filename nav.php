21672591ed0bf2fb3af31d95be674e06c03fbd2c
luc0liv <luoliveiradacunha@gmail.com>
2021-01-27 16:31:41 -0300

    Uso do collapse - bootstrap - precisa corrigir

---
+++ nav.php
@@ -0,0 +1,37 @@
+
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="./assets/css/main.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/comoFunciona.css">
+    <script defer type="text/javascript" src="./assets/scripts/index.js"></script>
+    <title>Como Funciona</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg ">
+   
+ <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapseNavbar"><span class="navbar-toggler-icon"></span></button>
+
+    <div class="collapse navbar-collapse" id="collapseNavbar">
+        <ul class="nav nav-fill">
+             <a class="navbar-brand" href="index.php"><img src="./assets/img/doatech.png" width="150px" alt="Logo Full Stack Eletro"></a>
+            <li class="nav-item "> 
+                <a class="nav-link active text-primary" href="index.php">Página Inicial</a> 
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active text-primary" href="comofunciona.php">Como funciona</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active text-primary" href="faleConosco.php">Fale Conosco</a>
+            </li>
+
+
+        </ul>
+    </div> 
+</nav>
+</body>
+</html>
